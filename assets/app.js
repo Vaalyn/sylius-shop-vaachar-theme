a94a565555e98c8e635f483a7effc83c4627365d
Vaalyn <vaalyndev@gmail.com>
2020-08-10 05:01:27 +0200

    Initial Commit

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+// Main scripts file
+import './js/index';
+
+// Main styles file
+import './scss/index.scss';
+
+// Images
+import './image/logo.png'
+import './image/logo-with-name.png'
+
+// Country Flags
+import 'flag-icon-css/css/flag-icon.css'
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+  .setOutputPath('public/assets/vaachar-theme')
+  .setPublicPath('/assets/vaachar-theme')
+  .addEntry('theme-entry', './themes/VaaCharTheme/assets/app.js')
+  .disableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableSassLoader()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction());
+
+const config = Encore.getWebpackConfig();
+config.name = 'vaaCharTheme';
+
+module.exports = config;
