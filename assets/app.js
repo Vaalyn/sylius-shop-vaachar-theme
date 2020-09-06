84d28e7478086e7b8566438711251b884d1fad92
Vaalyn <vaalyndev@gmail.com>
2020-09-06 22:47:36 +0200

    Add favicon and render channel name as store name

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,9 @@ import './scss/index.scss';
 import './image/logo.png'
 import './image/logo-with-name.png'
 
+// Favicons
+import './image/favicon/apple-touch-icon.png'
+import './image/favicon/favicon.ico'
+
 // Country Flags
 import 'flag-icon-css/css/flag-icon.css'
